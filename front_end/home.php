3a067173124d4b81e398907ac64139ba1118469b
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2020-06-02 14:14:03 +0530

    css file added footer added

--- front_end/home.php
+++ front_end/home.php
@@ -6,6 +6,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -25,8 +26,12 @@
   			</div>
 		</nav> 
 
+			
+		<!-- footer -->   
+		<footer>
+			<p> Copyright </p>
+		</footer>
 
-		<!-- footer -->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       <script src= "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
