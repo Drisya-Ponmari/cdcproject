516f1831cc5f55aa9e2dddca7d2521e10cf6e841
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2020-06-02 12:53:02 +0530

    navigation bar created

---
+++ front_end/about.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+
+<body>
+		<!--Navigation bar -->
+		<nav class="navbar navbar-inverse">
+  			<div class="container-fluid">
+    		<div class="navbar-header">
+      		<a class="navbar-brand" >Placement Cell</a>
+    		</div>
+    		<ul class="nav navbar-nav navbar-right">
+      			<li><a href="home.php">Home</a></li>
+      			<li class="active"><a href="about.php">AboutIITPKD</a></li>
+      			<li><a href="forstudents.php">ForStudent</a></li>
+      			<li><a href="forcompany.php">ForCompany</a></li>
+      			<li><a href="login.php">Login</a></li>
+    		</ul>
+  		</div>
+		</nav> 
+
+
+		<!-- footer -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src= "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+
+
+
+
