f9aa51fd1143f75c81398d8f1d5bb5a2b508f1a9
Vipin Kumar Seth <vipin.official.seth@gmail.com>
2020-06-10 22:32:46 +0530

    Added carousel

--- front_end/home.php
+++ front_end/home.php
@@ -9,6 +9,18 @@
   	<link rel="stylesheet" href="/css/style.css">
   	<link rel="stylesheet" type="text/css" href="/loginstyle/util.css">
 	<link rel="stylesheet" type="text/css" href="/loginstyle/main.css">
+	
+	
+	<style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 85vh;
+  }
+  </style>
+	
+	
+	
 </head>
 <body>
 		<!--Navigation bar -->
@@ -26,7 +38,40 @@
   			</div>
 		</nav> 
 
-			
+			<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/images/4.jpg" alt="Image" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="/images/2.jpg" alt="Image" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="/images/5.jpg" alt="Image" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="/images/6.jpg" alt="Image" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
 		<!-- footer -->   
 		<footer>
 			<p> Copyright </p>
